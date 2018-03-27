bb0914ad208906c9ce36df68ce4e059d275a5b93
Mergud <34305136+Mergud@users.noreply.github.com>
2018-03-27 13:41:43 +0300

    Update routes.js

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 
 router.use('/users', require('./users'));
+router.use('/orders', require('./orders'));
+router.use('/common_order', require('./common_order'));
+router.use('/menu', require('./menu'));
 
 module.exports = router;
